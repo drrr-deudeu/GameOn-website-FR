705e8a261cd75c3fcaaabda89e9530a8855d470f
drrr-deudeu <denis.rouiller@gmail.com>
2022-03-24 17:47:21 +0100

    responsive...

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -6,9 +6,6 @@ function editNav() {
     x.className = "topnav";
   }
 }
-let params = (new URL(document.location)).searchParams;
-let pbirthdate = params.get("birthdate");
-console.log("param birthdate :"+pbirthdate);
 
 // DOM Elements
 const modalbg = document.querySelector(".bground");
@@ -20,15 +17,21 @@ const modalBody = document.querySelectorAll(".modal-body");
 const closeBtn = document.querySelectorAll(".close");
 const formEl = document.getElementById("reserve");
 const confirmEl = document.querySelectorAll(".bground");
+const locationEl = document.querySelectorAll('[name="location"]');
 
-function testNothing(value){
-  return true;
-}
-const ageMinimum = 18;
-const ageMaximum = 120;
+
+const ageMinimum = 0;
+const ageMaximum = 140;
 const today = new Date();
 const minYear = today.getFullYear() - ageMinimum;
 
+// Map de gestion des champs du formulaire:
+// id du champ : 
+// {fonction d'init,
+// fonction de vérification,
+// type d'évènement,
+// message d'erreur,
+// contenu de la dernière valeur pour le fetch}
 const verifArray={
   'first': {
     'init' : clearInputContent,
@@ -73,27 +76,20 @@ const verifArray={
     'last-value': ""
   },
   'checkbox1': {
-    'init' : clearInputContent,
+    'init' : clearCheckBox,
     'verif': testChecked,
     'event': onchange,
     'data-error': "Vous devez vérifier que vous acceptez les termes et conditions.",
     'last-value': ""
   },
   'checkbox2': {
-    'init' : clearInputContent,
+    'init' : clearCheckBox,
     'verif': testNothing,
     'event': onchange,
     'data-error': "",
     'last-value': ""
   },
 };
-
-
-formData.forEach(function(f){
-  let field = f.querySelector('input').getAttribute('id');
-  f.querySelector('input').addEventListener(verifArray[field]['event'],verifField);
-  f.setAttribute('data-error',verifArray[field]['data-error']);
-});
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -105,6 +101,7 @@ closeBtn.forEach((btn) => btn.addEventListener("click", closeModal));
 
 // launch modal form
 function launchModal() {
+  // Appel des fonctions d'initialisation des champs
   formData.forEach(function(f){
     let field = f.querySelector('input').getAttribute('id');
     initFieldElement(document.getElementById(field));
@@ -117,34 +114,54 @@ function closeModal() {
   modalbg.style.display = "none";
 }
 
-// close modal form
+// close modal confirmation form 
 function closeModal1() {
   modalbg1.style.display = "none";
 }
 
+//
+// FONCTIONS DE VALIDATION DES CHAMPS
+//
+
+// First and Last Name : même fonction de vérification
 function testFirstAndLastName(el){
   const verif = /.{2,}/;
   let ret = verif.test(el.value);
-//  console.log("test:"+el.value+" ret:"+ret);
   return ret;
 }
+
+// Fonction de test du nombre, sans limite...
 function testNumber(el){
   const verif = /\d{1,}/; /* equivalent à /[0,9]{1,}/ */
   return(verif.test(el.value));
 }
-// via input type="email". Regexp instead?
-function testEmail(el){
-  if(!el.value.length)
-    return false;
-  if(el.validity.typeMismatch){
-    return false;
-  }
+
+// 2 champs sont concernés par cette fonction:
+// 1. les radio-boutons, qui sont testés lors du submit
+// 2. la case de la newsletter
+function testNothing(value){
   return true;
 }
 
-// regexp ou fonction?
+// enlever input type="email". Regexp instead
+// Source : https://www.w3resource.com/javascript/form/email-validation.php
+function testEmail(el){
+  const verif = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  return(verif.test(el.value));
+  // Le check ci-dessous est pas mal car les règles seront updatées automatiquement 
+  // en cas d'update des règles au niveau du browser
+  // ... mais ne convient pas car il affiche aussi la bulle par défaut 
+  // quand le bouton de submit du formulaire est activé 
+  // if(!el.value.length)
+  //   return false;
+  // if(el.validity.typeMismatch){
+  //   return false;
+  // }
+  // return true;
+}
+
+// validation via les fonctions Date de javascript
 function testBirthDate(el){
-//  console.log("birthdate:"+el.value);
   if(!el.value.length)
     return false;
   let date = new Date(el.value);
@@ -155,10 +172,10 @@ function testBirthDate(el){
   let diffYear = diff.getFullYear() - 1970;
   if(diffYear<ageMinimum || diffYear>ageMaximum)
     return false;
-  // console.log("birthdate true:"+el.value);
   return true;
 }
 
+// validation des coches
 function testChecked(el){
   if(el.checked){
     return true;
@@ -166,25 +183,56 @@ function testChecked(el){
   return false;
 }
 
+// fonctions d'initialisation des values champs
 function clearInputContent(el){
   el.value = "";
 }
 
+// Fonction de vérification des radio-boutons.
+// Au moins 1 doit être checké
+function radioButtonsChecked(){
+  let checked = false;
+  let town = "";
+  // c'est dommage de ne pas pouvoir sortir du forEach quand on veut... à améliorer
+  locationEl.forEach(function(loc){
+    if(loc.checked){
+      checked = true;
+      town = loc.value;
+    }
+  });
+  if(checked){
+    locationEl[0].parentNode.setAttribute('data-error-visible','false');
+    verifArray['location1']['last-value'] = town;
+    return true;
+  }
+  locationEl[0].parentNode.setAttribute('data-error-visible','true');
+  return false;
+}
+
+//
+// FONCTIONS D'INITIALISATION DES CHAMPS
+//
+
+// aucune ville choisie par défaut
 function defaultLocation(el){
-
+  locationEl.forEach(function(loc){
+    loc.checked = false;
+  });
 }
 
+// la case conditions d'utilisation est décochée par défaut
 function clearCheckBox(el){
-
+  el.checked = false;
 }
 
+// fonction d'initialisation des champs
 function initFieldElement(el){
+  el.parentNode.setAttribute('data-error-visible','false');
   verifArray[el.getAttribute('id')]['init'](el);
 }
 
+// fonction de validation commune aux évènements et au submit
 function verifFieldElement(el){
-//  console.log(el.getAttribute('id'));
-
   if(verifArray[el.getAttribute('id')]['verif'](el)){
     el.parentNode.setAttribute('data-error-visible','false');
     verifArray[el.getAttribute('id')]['last-value'] = el.value;
@@ -193,32 +241,20 @@ function verifFieldElement(el){
   el.parentNode.setAttribute('data-error-visible','true');
   return false;
 }
+
+// même fonction listener pour tous les éléments
 function verifField(e){
   return verifFieldElement(e.target);
 }
 
-function radioButtonsChecked(){
-  let location = document.querySelectorAll('[name="location"]');
-  let checked = false;
-  let town = "";
+// Mise en place des listeners de chaque champ du formulaire
+formData.forEach(function(f){
+  let field = f.querySelector('input').getAttribute('id');
+  f.querySelector('input').addEventListener(verifArray[field]['event'],verifField);
+  f.setAttribute('data-error',verifArray[field]['data-error']);
+});
 
-  location.forEach(function(loc){
-    // console.log("Location:"+loc.value+" checked:"+loc.checked);
-    if(loc.checked){
-      checked = true;
-      town = loc.value;
-    }
-  });
-  if(checked){
-    location[0].parentNode.setAttribute('data-error-visible','false');
-    verifArray['location1']['last-value'] = town;
-  //  console.log("TOWN:"+town);
-    return true;
-  }
-  location[0].parentNode.setAttribute('data-error-visible','true');
-  return false;
-}
-
+// listener sur submit
 // traitement des validités avant envoi
 formEl.addEventListener("submit", function (event) {
   let ret = true;
@@ -248,7 +284,6 @@ formEl.addEventListener("submit", function (event) {
   // Dans ce cas il faut faire le event.preventDefault
   // voilà la string originale envoyée automatiquement par le submit:
   // first=dd&last=dd&email=dd%40d.org&birthdate=2000-02-18&quantity=1&location=Portland
-  // console.log("window.location.href:" + window.location.href);
   var url = new URL(window.location.href);
   Object.keys(verifArray).forEach(function(k){
     if(k==='location1')
@@ -263,7 +298,7 @@ formEl.addEventListener("submit", function (event) {
     .then((response) => console.log("fetch ok?"+response.ok+response.status))
     .catch(e => console.log("something went wrong: " + e))
   
-  // on affiche la fenetre d'inscription effectuée si reponse serveur ok (bon ici il n'y a pas vraiment de serveur)
+  // on affiche la fenetre d'inscription effectuée si reponse serveur ok (bon ici il n'y a pas de serveur)
   modalbg1.style.display = "block"; 
   modalbg.style.display = "none";
 
